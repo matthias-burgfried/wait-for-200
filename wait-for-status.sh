fef85acdebd3bc4c0e1e3eb3298cf0acccab5178
Matthias Burgfried <matthias@infojungs.de>
2020-04-17 16:05:44 +0200

    Update wait-for-status.sh

--- wait-for-status.sh
+++ wait-for-status.sh
@@ -4,8 +4,8 @@ TIMEOUT="${TIMEOUT:-600}"
 STATUS="${STATUS:200}"
 seconds=0
 
-echo 'Waiting up to' $TIMEOUT 'seconds for HTTP 200 from' $URL 
-until [ "$seconds" -gt "$TIMEOUT" ] || $((curl -L --silent --max-time $TIMEOUT --head --fail $URL 2>/dev/null | head -n 1|cut -d$' ' -f2) == $STATUS); do
+echo 'Waiting up to' $TIMEOUT 'seconds for HTTP $STATUS from' $URL 
+until [ "$seconds" -gt "$TIMEOUT" ] || "$(curl -L -s -o /dev/null --max-time $TIMEOUT -I -w "%{http_code}" $URL)" == "$STATUS" ]; do
   printf '.'
   sleep 5
   seconds=$((seconds+5))
@@ -14,6 +14,6 @@ done
 if [ "$seconds" -lt "$TIMEOUT" ]; then
   echo 'OK'
 else
-  echo "ERROR: Timed out wating for HTTP 200 from" $URL >&2
+  echo "ERROR: Timed out wating for given HTTP Status from" $URL >&2
   exit 1
 fi
